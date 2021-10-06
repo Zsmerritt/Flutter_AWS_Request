ebdb5fa54454f943147506daec83236d39633b9f
=Zachary Merritt <=zsmerritt@gmail.com>
2021-10-05 22:34:54 -0700

    Revert "started rework into smaller more testable chunks"
    This reverts commit 2e7fafb2f8916693a98e9feb712ca9accad3dec4.

--- lib/testing.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Zachary Merritt.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// MIT license that can be found in the LICENSE file.
-
-library MockAwsRequest;
-
-export 'src/mock_aws_request.dart' show MockAwsRequest;
-export 'src/util.dart' show AwsRequestType, AwsRequestException;
